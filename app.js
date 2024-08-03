2ad6a4a1febd8b2e826eae63295a3d3e9a3d60b5
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-08-03 08:21:57 +0200

    learn modularity

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const sayHello = (name) => {
-  console.log("Hello", name);
-};
+console.log(); // global
 
-sayHello(window);
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+// window.console.log = console.log (in the browser)
+
+var message = '';
+window.message // message available in the browser
+global.message // in node global replaces the window, but variables are not attached to it. so this does not work
