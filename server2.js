6eb4bc6729b667925a5a343bf2458ee469bb3058
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-08-04 16:09:53 +0200

    logger middleware

--- server2.js
+++ server2.js
@@ -8,34 +8,37 @@ const users = [
   { id: 3, name: "Mario Doe" },
 ];
 
-// const server = createServer((req, res) => {
-//   if (req.url === "/api/users" && req.method === "GET") {
-//     res.setHeader("Content-Type", "application/json");
-//     res.write(JSON.stringify(users));
-//     res.end();
-//   }
-// });
+// Logger middleware
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+};
 
 const server = createServer((req, res) => {
-  if (req.url === "/api/users" && req.method === "GET") {
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify(users));
-  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
-    const id = req.url.split("/")[3];
-    const user = users.find((user) => user.id === parseInt(id));
-    if(user) {
+  logger(req, res, () => {
+    if (req.url === "/api/users" && req.method === "GET") {
       res.setHeader("Content-Type", "application/json");
       res.writeHead(200);
-      res.end(JSON.stringify(user));
+      res.end(JSON.stringify(users));
+    } else if (
+      req.url.match(/\/api\/users\/([0-9]+)/) &&
+      req.method === "GET"
+    ) {
+      const id = req.url.split("/")[3];
+      const user = users.find((user) => user.id === parseInt(id));
+      if (user) {
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end(JSON.stringify(user));
+      } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("User Not Found");
+      }
     } else {
       res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("User Not Found");
+      res.end("Not Found");
     }
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
-  }
+  });
 });
 
 server.listen(PORT, () => {
