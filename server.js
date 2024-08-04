d91ea924ec367f578e2102f24d05e19004128564
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-08-04 10:49:05 +0200

    req.url and req.method

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const PORT = process.env.PORT;
 const server = http.createServer((req, res) => {
   // res.setHeader('Content-Type', 'text/plain');
   // res.statusCode = 404;
+  console.log("req.url:", req.url);
+  console.log("req.method:", req.method);
 
-
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  res.end('<h1>Hello Mars</h1>');
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello Mars</h1>");
 });
+
+app.post('/user', () => {}) // this only runs when there is a post erquest to the user. this is express
 
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
