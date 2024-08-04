b7b7ddf0d7b84d5ddaf61fe3483ed3df30c0357c
Gregor Schafroth <gregor.schafroth@gmail.com>
2024-08-04 15:03:07 +0200

    also pushing server.js

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 import http from "http";
-const PORT = process.env.PORT;
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
+const PORT = process.env.PORT || 8000;
+
+// Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("__filename", __filename)
+console.log("__dirname", __dirname)
+
 
 const server = http.createServer((req, res) => {
-  // res.setHeader('Content-Type', 'text/plain');
-  // res.statusCode = 404;
-  console.log("req.url:", req.url);
-  console.log("req.method:", req.method);
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Hello Mars</h1>");
+  try {
+    // Check if GET request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>Homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>About</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("<h1>Not Found</h1>");
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
 });
-
-app.post('/user', () => {}) // this only runs when there is a post erquest to the user. this is express
 
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
